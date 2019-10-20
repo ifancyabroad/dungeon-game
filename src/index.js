e979d7f1eed09c0b259e35e31287150b0998a733
ifancyabroad <edgar.nightingale88@gmail.com>
2019-10-20 17:30:34 +0100

    Add world collision detection

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
+      gravity: { y: 0 },
       debug: false
     }
   }
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -10,9 +10,13 @@ export class Game extends Phaser.Scene {
     const room = this.make.tilemap({ key: 'room' });
     const tileset = room.addTilesetImage('0x72_DungeonTilesetII_v1.3', 'tiles');
 
-    const belowLayer = room.createStaticLayer('Below Player', tileset, 0, 0).setScale(2);
+    this.belowLayer = room.createStaticLayer('Below Player', tileset, 0, 0).setScale(2);
     this.player = new Hero(this, this.game.config.width / 2 - 16, this.game.config.height / 2 - 16, 'player');
-    const worldLayer = room.createStaticLayer('World', tileset, 0, 0).setScale(2);
+    this.worldLayer = room.createStaticLayer('World', tileset, 0, 0).setScale(2);
+
+    // Add collision between player and world
+    this.worldLayer.setCollisionFromCollisionGroup();
+    this.physics.add.collider(this.player, this.worldLayer);
 
     // Create movement keys
     this.cursorKeys = this.input.keyboard.createCursorKeys();
