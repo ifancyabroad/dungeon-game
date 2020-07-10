c3d011ec61bf3840069cda2d7ba7ef49accb4390
ifancyabroad <edgar.nightingale88@gmail.com>
2020-07-10 09:16:05 +0100

    Resize game

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { Room } from "./scenes/Room";
 const config = {
   type: Phaser.AUTO,
   scale: {
+    parent: 'game-container',
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
     width: 400,
