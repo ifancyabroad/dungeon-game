fde66fb04e8cd8b345d6ee7eb0264065f0a245a0
ifancyabroad <edgar.nightingale88@gmail.com>
2020-07-22 12:28:17 +0100

    Update game over font

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -5,19 +5,24 @@ export class GameOver extends Phaser.Scene {
   }
 
   create() {
-    this.add.text(this.game.renderer.width / 2, 100, 'GAME OVER', {
-      fontFamily: '"Roboto Condensed"',
+    this.cameras.main.setBackgroundColor('#000000');
+
+    this.add.text(this.game.renderer.width / 2, 120, 'GAME OVER', {
+      fontFamily: 'EquipmentPro',
       fontSize: '24px',
       strokeThickness: 1
     }).setOrigin(0.5);
 
-    this.add.text(this.game.renderer.width / 2, 200, '(PRESS SPACEBAR TO RETURN TO MENU)', {
-      fontFamily: '"Roboto Condensed"',
+    this.add.text(this.game.renderer.width / 2, 140, '(PRESS SPACEBAR TO RETURN TO MENU)', {
+      fontFamily: 'EquipmentPro',
       fontSize: '16px',
       // strokeThickness: 1
     }).setOrigin(0.5);
 
     this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+    // Fade in
+    this.cameras.main.fadeIn(600);
   }
 
   update() {
